f696263ba7c073d12f875e2c0499fd320304f8ba
Jafar Shaik <shaikjafar2010@gmail.com>
2025-08-21 23:28:30 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 @app.route("/")
 def home():
-    return render_template("index.html")
+    name = request.args.get("name")
+    return render_template("index.html", name=name)
+@app.route("/about")
+def about():
+    return render_template("about.html")
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run()
